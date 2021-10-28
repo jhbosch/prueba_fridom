c92dfa8c2982a41543daab16ed598fb3c350a90d
jhbosch <jesus.hbosch@gmail.com>
2021-10-28 02:26:46 -0400

    add navigation

---
+++ src/screen/Details/Details.js
@@ -0,0 +1,24 @@
+import './Details.css';
+
+function Details() {
+  return (
+    <div className="Details">
+      <header className="App-header">
+        
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>  
+  );
+}
+
+export default Details;
